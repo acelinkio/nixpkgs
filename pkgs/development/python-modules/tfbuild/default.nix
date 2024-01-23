23552750810e3030b13a9e86a97b11b714a1b8a4
Arlan Lloyd <arlanlloyd@gmail.com>
2024-01-23 20:49:39 +0000

    add tfbuild

---
+++ pkgs/development/python-modules/tfbuild/default.nix
@@ -0,0 +1,46 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, pythonOlder
+
+# build-system
+, setuptools
+
+# tests
+, pytestCheckHook
+}:
+
+buildPythonPackage rec {
+  pname = "tfbuild";
+  version = "1.3.1";
+  pyproject = true;
+
+  disabled = pythonOlder "3.6";
+
+  src = fetchFromGitHub {
+    owner = "mpearson117";
+    repo = pname;
+    rev = "refs/tags/v${version}";
+    hash = "";
+  };
+
+  nativeBuildInputs = [
+    setuptools
+  ];
+
+  nativeCheckInputs = [
+    pytestCheckHook
+  ];
+
+  pythonImportsCheck = [
+    "tfbuild"
+  ];
+
+  meta = with lib; {
+    description = "Extensible memoizing collections and decorators";
+    homepage = "https://github.com/mpearson117/tfbuild";
+    changelog = "https://github.com/mpearson117/tfbuild/releases/tag/v${version}";
+    license = with licenses; [ asl20 ];
+    maintainers = with maintainers; [ acelinkio ];
+  };
+}
